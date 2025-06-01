84a6b4f729dbd88940c7d266afbe1e5dcabfdbb8
keshavpj1711 <keshavpj1711@gmail.com>
2025-06-01 17:38:07 +0530

    add: defined the /search endpoint with search functionality to be added

--- app/main.py
+++ app/main.py
@@ -18,7 +18,9 @@ app = FastAPI(
 # Defining endpoints
 
 # / : root endpoint, this will be a greeting 
-@app.get("/")
+@app.get(
+  "/", 
+  tags=["Root"])
 async def greet_user():
   return {
     "message": "Welcome!"
@@ -60,3 +62,26 @@ async def add_document(article_data: ArticleCreate):
     content=article_data.content,
     retrieved_at=mock_retrieved_at
   )
+
+@app.get(
+  "/search", 
+  summary="Search for documents",
+  tags=["Search"],
+)
+async def search_documents(query: str):
+  print(f"Recieved search query: '{query}'")
+
+  # Later on we will process this query 
+  # Use the TF-IDF scores to search for matching documents
+  # Rank the results
+  # Return the list of documents
+
+  mock_results = [
+    {"id": 101, "title": "Mock Result 1 for " + query, "relevance_score": 0.95, "url": "http://example.com/result1"},
+    {"id": 102, "title": "Another Mock Result for " + query, "relevance_score": 0.87, "url": "http://example.com/result2"}
+  ]
+
+  return {
+    "query recieved": query,
+    "search results": mock_results,
+  }
