1c201a7bcb3a7c099d5270a705f976bf60099ada
Ritwik Singh <97968834+datasciritwik@users.noreply.github.com>
2025-06-08 11:23:16 +0000

    Added: Auth for all the routes

---
+++ app/models/user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+# Pydantic models
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    full_name: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class User(BaseModel):
+    id: int
+    email: str
+    full_name: str
+    is_active: bool = True
+
+class UserInDB(User):
+    hashed_password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
